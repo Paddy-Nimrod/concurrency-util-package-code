41d67524d759c5480d79a6e4fce0bfab0c0b4a0a
Paddy-Nimrod <n.ambetsa21@gmail.com>
2024-07-13 00:15:45 +0300

    added myConsumer thread function

--- src/Main.java
+++ src/Main.java
@@ -32,5 +32,44 @@ class MyProducer implements Runnable {
             }
         }
 
+        System.out.println(color + "Adding EOF and exiting...");
+        buffer.add("EOF");
+
     }
 }
+
+
+class MyConsumer implements Runnable {
+    private List<String> buffer;
+    private String color;
+
+    public MyConsumer(List<String> buffer, String color) {
+        this.buffer = buffer;
+        this.color = color;
+    }
+
+    public void run() {
+        while (true) {
+            if (buffer.isEmpty()) {
+                continue;
+            }
+            if (buffer.get(0).equals(EOF)){
+                System.out.println(color + "Exiting...");
+                break;
+            }else {
+                System.out.println(color + "Removed " + buffer.remove(0));
+            }
+
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
